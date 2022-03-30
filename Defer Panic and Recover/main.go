9aa1fe4815d6a3d52e2f38fbf7be5586a2b506ea
Nicolas Toews <toewsnicolas@gmail.com>
2022-03-30 11:42:10 -0300

    Ejemplo Defer Panic Recover

--- Defer Panic and Recover/main.go
+++ Defer Panic and Recover/main.go
@@ -9,7 +9,7 @@ func main() {
 
 func f() {
 	defer func() {
-		if r := recover(); r != nil {
+		if r := recover(); r != nil { //r toma el valor del recover y como es distinto de nil, continua
 			fmt.Println("Recuperando en f", r)
 		}
 	}()
@@ -21,9 +21,9 @@ func f() {
 func g(i int) {
 	if i > 3 {
 		fmt.Println("Panicking!")
-		panic(fmt.Sprintf("%v", i))
+		panic(fmt.Sprintf("%v", i)) //Una vez i llegue a 4 le damos un panic y vuelve a la funcion f
 	}
 	defer fmt.Println("Defer en g", i)
 	fmt.Println("Imprimiendo g", i)
-	g(i + 1)
+	g(i + 1) //al llamar a la misma función dentro de si misma se genera un bucle, esta funcion se va a repetir hasta que i valga 4, por el if de mas arriba
 }
